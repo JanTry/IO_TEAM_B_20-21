d6159f7de626d41560cf6f67dcbb253c81a47463
kpodsiadlo <kamilpodsiadlo44@gmail.com>
2021-03-21 23:35:59 +0100

    socket io get started

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(__dirname + '/static/chat.html');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.broadcast.emit('hi');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
