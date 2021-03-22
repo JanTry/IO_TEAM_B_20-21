4f566dea7dfd4f0d9643ed7f06c538aa53970329
Hubert Pelczarski <pelczarskihubert@gmail.com>
2021-03-22 17:08:36 +0100

    initial socket io socket in react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
-    const socket = io();
-    let messages: any[] = []
-    
-    const submitForm = (event: any) => {
-      event.preventDefault();
-      if (event.target.value) {
-          socket.emit('chat message', event.target.value);
-          event.target.value = '';
-      }
-    }
+  const [response, setResponse] = useState("");
+  const socket = socketIOClient(ENDPOINT);
 
-    socket.on('chat message', msg: any => {
-      const item = document.createElement('li');
-      item.textContent = msg;
-      messages = messages.concat(msg)
-      window.scrollTo(0, document.body.scrollHeight);
-  });
+  useEffect(() => {
+    socket.on("chat message", (data:any) => {
+      setResponse(data);
+    });
+  }, [socket]);
+
+
+  const submitForm = (event: any) => {
+    event.preventDefault();
+    if (event.target.value) {
+        socket.emit('chat message', event.target.value);
+        event.target.value = '';
+    }
+  }
+
+  // socket.on('chat message', msg: any) => {
+  //   const item = document.createElement('li');
+  //   item.textContent = msg;
+  //   messages = messages.concat(msg)
+  //   window.scrollTo(0, document.body.scrollHeight);
+  // });
 
   return (
-
-  <div>
-    <ul>
-      {
-        messages.map(message => 
-          <li>message
-
-          </li>)
-      }
-    </ul>
-    <form onSubmit={submitForm}>
-      <input id="input"/><button>Send</button>
-    </form>
-
-    
-  </div>
+    <div>
+      <p>{response}</p>
+    </div>
   );
 }
 
