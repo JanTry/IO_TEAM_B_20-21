21b5906c81f2314b1d5ef9ff40c67545b07cf88a
hubert <pelczarskihubert@gmail.com>
2021-03-24 12:19:15 +0100

    feat: initial docker setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import socketIOClient from "socket.io-client";
 
-const socket = socketIOClient('http://localhost:4000');
+const socket = socketIOClient('http://localhost:8080');
 
 function App() {
   const [messages, setMessages] = useState([]);
