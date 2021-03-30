69ca460a90951d582fa4cc8e16c846563d996e08
hubert <pelczarskihubert@gmail.com>
2021-03-31 01:40:18 +0200

    restored old ports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import socketIOClient from "socket.io-client";
 
-const socket = socketIOClient('http://localhost:8080');
+const socket = socketIOClient('http://localhost:4000');
 
 function App() {
   const [messages, setMessages] = useState([]);
