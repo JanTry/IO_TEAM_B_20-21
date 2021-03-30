641c72bfeb0315fd910b5da29e3827dee4157f12
hubert <pelczarskihubert@gmail.com>
2021-03-31 00:49:34 +0200

    feat: backend <-> mongo docker connection

--- backend/src/database/dbUtils.ts
+++ backend/src/database/dbUtils.ts
@@ -1,26 +1,22 @@
 import mongoose = require("mongoose");
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
 
-export const connect = (databaseName=process.env.DATABASE_NAME, address=process.env.DATABASE_ADDRESS, port=process.env.DATABASE_PORT) => {
-    try{
-        const url = `mongodb://${address}:${port}/${databaseName}`
-        mongoose.connect(url,{
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        })
-        console.log("CONNECTION ESTABLISHED\n")
-    } catch (error) {
-        console.log("ERROR during connecting to database : ", error)
-    }
-}
+dotenv.config();
 
-export const disconnect = () => {
-    try{
-        mongoose.disconnect()
-    } catch (error) {
-        console.log("ERROR during disconnecting from database : ", error)
-    }
-}
-
+export const connect = (
+  databaseName = process.env.DATABASE_NAME,
+  address = process.env.DATABASE_ADDRESS,
+  port = process.env.DATABASE_PORT
+) => {
+  const url = `mongodb://${address}:${port}/${databaseName}`;
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.connection
+    .on("error", console.error.bind(console, "DATABASE CONNECTION ERROR"))
+    .once("open", () => {
+      console.log("DATABASE CONNECTED");
+    });
+};
