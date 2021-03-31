ace54dd34e23bdc8ba750452cfbbde1941df5703
WojtAcht <wachtelik@gmail.com>
2021-03-31 08:59:23 +0200

    Start implementing auth endpoints

--- backend/src/database/models/user.ts
+++ backend/src/database/models/user.ts
@@ -3,13 +3,24 @@ import * as mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: true
     },
     surname: {
         type: String,
         required: false
-    }
-}, {
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8
+    }},
+    {
     collection: 'users'
 })
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,11 @@ import http from "http";
 import {Server} from "socket.io";
 import cors from 'cors';
 import dotenv  from "dotenv";
+import {User} from './database/models/user'
 import * as db from './database/dbUtils';
+import { body, validationResult } from "express-validator";
+import {ObjectId} from 'mongodb';
+import * as users from './database/collectionsUtils/userUtils'
 db.connect()
 
 dotenv.config()
@@ -35,3 +39,46 @@ io.on('connection', (socket) => {
 httpServer.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+app.use(express.json());
+
+app.post('/api/v1/register',
+          body('email').isEmail(),
+          body('password').isLength({ min: 8 }),
+          (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+            }
+            const userData = {email: req.body.email,
+                              password: req.body.password,
+                              id: new ObjectId()};
+            console.log(userData);
+            const result = users.addUser(userData);
+            console.log(result);
+            res.status(200).send('Welcome to login , sign-up api');
+          });
+
+app.post('/api/v1/auth/login',
+          body('email').isEmail(),
+          body('password').isLength({ min: 8 }),
+          (req,res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+            }
+            const password = User.findOne({password: req.body.password});
+            if(password === req.body.password){
+              res.status(200).send('Welcome to login , sign-up api');
+            }
+            else{
+              res.status(400).json({errors: [{auth: "Invalid password or user"}]});
+            }
+
+          });
+
+app.post('/api/v1/auth/create-user',
+          (req,res) => {
+            res.status(200).send('Welcome to login , sign-up api');
+          });
+
