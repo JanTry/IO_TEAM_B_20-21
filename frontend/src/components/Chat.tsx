e74ca4498b507721f69295afb57fbb1eea90c7b8
szarbartosz <szarbartosz@gmail.com>
2021-04-03 11:46:49 +0200

    fix: fixed bug with useRef hook

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import io from "socket.io-client";
 import './styles/Chat.css';
+
+const socket = io.connect("http://localhost:4000");
 
 interface ChatMessage {
   from: string,
@@ -10,52 +12,52 @@ interface ChatMessage {
 
 const Chat = () => {
   const history = useHistory();
+  const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([] as string[]);
   const [userID, setUserID] = useState('');
   const [sessionID, setSessionID] = useState('');
   const [accessCode, setAccessCode] = useState('');
   const [connected, setConnected] = useState(false);
 
-  const socket: any = useRef()
-
-  useEffect(() => {
-    socket.current = io.connect("http://localhost:4000")
-    socket.current.on("chat-message", ({from, msg}: ChatMessage) => {
-      console.log(messages)
-      setMessages(messages.concat(`${from}: ${msg}`))
-    })
-    return () => socket.current.disconnect()
-  }, [messages])
-
   useEffect(() => {
     const { userID, sessionID, accessCode } = history.location.state as { [key: string]: string };
     setUserID(userID);
     setSessionID(sessionID);
     setAccessCode(accessCode);
+
+    socket.emit('join', { userID, sessionID, accessCode }, (response: any) => {
+      if (response.status === "ok") {
+        setConnected(true)
+      } else {
+        console.error(response.msg)
+      }
+    })
   }, [history.location.state])
+
+
+
+  useEffect(() => {
+    socket.on("chat-message", ({ from, msg }: ChatMessage) => {
+      setMessage(`${from}: ${msg}`);
+    })
+  }, [message]);
+
+  useEffect(() => {
+    if (message !== '') {
+      setMessages(messages.concat(message))
+    }
+  }, [message])
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
     if (event.target.message.value) {
-      socket.current.emit('chat-message', event.target.message.value);
+      socket.emit('chat-message', event.target.message.value);
       event.target.message.value = '';
     }
   }
 
-  const connect = () => {
-    socket.current.emit('join', { userID, sessionID, accessCode }, (response: any) => {
-      if (response.status === "ok") {
-        setConnected(true)
-      }
-      else {
-        console.error(response.msg)
-      }
-    })
-  }
-
   return (
     <div>
-      <button onClick={connect}>Connect to the server</button>
       <div>
         userID: {userID}, sessionID: {sessionID}, accessCode: {accessCode}, connected: {connected ? "yes" : "no"}
       </div>
