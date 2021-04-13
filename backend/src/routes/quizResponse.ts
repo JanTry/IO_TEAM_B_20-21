870fe5cbf0a74fc6871594b738ff78a96db1738f
kpodsiad <kamilpodsiadlo44@gmail.com>
2021-04-13 14:16:41 +0200

    fix: review fixes

--- backend/src/routes/quizResponse.ts
+++ backend/src/routes/quizResponse.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { body, CustomValidator, validationResult } from 'express-validator';
-import get from 'lodash.get';
 import { QuizResponse } from '../database/models/quizResponse';
 import { User } from '../database/models/user';
 import { Quiz } from '../database/models/quiz';
@@ -8,7 +7,7 @@ import { Quiz } from '../database/models/quiz';
 export const quizResponseRoutes = express.Router();
 
 quizResponseRoutes.get('/', (req, res) => {
-    QuizResponse.find({}, (err, results) => {
+  QuizResponse.find({}, (err, results) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -16,12 +15,6 @@ quizResponseRoutes.get('/', (req, res) => {
     }
   });
 });
-
-const validAnswersValidator: CustomValidator = (validAnswersIds, meta) => {
-  const path = `${meta.path.split('.').splice(0, 1).join('.')}.answers`;
-  const answers = get(meta.req.body, path);
-  return validAnswersIds.every((id) => answers.find((a) => a._id === id) != null);
-};
 
 const studentIdValidator: CustomValidator = async (studentId) => {
   return new Promise<void>((resolve, reject) => {
@@ -33,30 +26,28 @@ const studentIdValidator: CustomValidator = async (studentId) => {
 };
 
 const quizIdValidator: CustomValidator = async (quizId) => {
-    return new Promise<void>((resolve, reject) => {
-        Quiz.findOne({ _id: quizId }).then((res: any) =>{
-            if (res != null) resolve();
-            else reject();
-        })
-    })
-}
+  return new Promise<void>((resolve, reject) => {
+    Quiz.findOne({ _id: quizId }).then((res: any) => {
+      if (res != null) resolve();
+      else reject();
+    });
+  });
+};
 
 quizResponseRoutes.post(
-    '/',
-    body('quizId').isMongoId().custom(quizIdValidator).withMessage('Invalid Quiz'),
-    body('studentId').isMongoId().custom(authorIdValidator).withmessage('Invalid user'),
-    body('questionResponses.*.questionId').isMongoId(),
-    body('questionResponses.*.answer').isMongoId(),
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-        QuizResponse.create(req.body, (err, result) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            return res.status(200).send(result);
-        });
-    }
-)
-
-
+  '/',
+  body('quizId').isMongoId().custom(quizIdValidator).withMessage('Invalid Quiz'),
+  body('studentId').isMongoId().custom(studentIdValidator).withMessage('Invalid user'),
+  body('questionResponses.*.questionId').isMongoId(),
+  body('questionResponses.*.answer').isMongoId(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    QuizResponse.create(req.body, (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      return res.status(200).send(result);
+    });
+  }
+);
