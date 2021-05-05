a14a5d73c3d6704a9c9bcf74faef4bf38bbffafd
Jan Try <44120531+JanTry@users.noreply.github.com>
2021-05-05 17:10:11 +0200

    Database population (#91)
    DB users mockup

--- backend/src/database/populationUtils/userPopulation.ts
+++ backend/src/database/populationUtils/userPopulation.ts
@@ -1,15 +1,20 @@
 /* eslint-disable no-console */
 
 import faker from 'faker';
+import * as bcrypt from 'bcrypt';
 import { addUser } from '../collectionsUtils/userUtils';
 
 export const populateUsersCollection = async (n = 10, role = 'student') => {
+  const ROUNDS = 10;
+  const salt = await bcrypt.genSalt(ROUNDS);
+  const password = await bcrypt.hash('password123', salt);
+
   for (let i = 1; i <= n; i++) {
     addUser({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       email: faker.internet.exampleEmail(),
-      password: faker.internet.password(10),
+      password,
       role,
     });
   }
